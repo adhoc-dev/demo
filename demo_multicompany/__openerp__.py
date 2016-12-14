66c68459dc6753baed698a5b5a8b78d2bd49f288
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-14 19:37:44 -0300

    ADD dashboard tile

--- demo_multicompany/__openerp__.py
+++ demo_multicompany/__openerp__.py
@@ -328,7 +328,7 @@
         # 'contract_multic_fix', DEPRECIADO
         'purchase_multic_fix',
         # 'sale_multic_fix', DEPRECIADO
-        # 'web_dashboard_tile', NO MIGRADO
+        'web_dashboard_tile',
         'web_easy_switch_company',
         'website_blog',
         'website_crm',
