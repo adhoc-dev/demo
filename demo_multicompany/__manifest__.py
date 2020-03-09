8a89fff5faacc172f89817de032b7eeca5b1de23
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-03-09 09:52:34 -0300

    [MIG] demo_multicompany/: Migration to 13.0

--- demo_multicompany/__manifest__.py
+++ demo_multicompany/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Demo of Multicompany Environment',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Tools',
     'sequence': 14,
     'summary': '',
@@ -135,7 +135,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
