6b68c9992d2dede80db055ccb1644d9a45bd9335
matiasperalta1 <matias_peralta1@hotmail.com>
2023-06-30 15:40:35 -0300

    [ADD] sale_tour: new sales tour

---
+++ sale_tour/tests/test_controller.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.tests import HttpCase, tagged
+
+
+@tagged('post_install', '-at_install')
+class TestSaleSignature(HttpCase):
+
+    def test_01_portal_sale_signature_tour(self):
+        self.start_tour("/web", 'sale_tour.sale', login="admin")
