c981c87fbcc72cd251969d612f4f55f8c89cc289
Matias Peralta <matias_peralta1@hotmail.com>
2023-10-20 16:07:14 -0300

    Nuevos cambios

--- sale_tour/static/src/js/tours/multicompany.js
+++ sale_tour/static/src/js/tours/multicompany.js
@@ -16,83 +16,103 @@ odoo.define('sale_tour.multicompany', function(require) {
     // Paso 7: Guardar la factura.
 
     const steps =  [tour.stepUtils.showAppsMenuItem(),
+        // {
+            // trigger: ".o_switch_company_menu > .dropdown-toggle",
+            // content: _t("Paso 1: Abrir desplegable"),
+            // position: "bottom",
+            // edition: "enterprise",
+            // run: function (actions) {
+            //         location.reload();
+            // },
+            // async run() {
+            //     location.reload();
+            //     await new Promise((r) => setTimeout(r, 10000));
+            // }
+        // },
+        // {
+        //     trigger: "div[title='Switch to Muebleria SRL']",
+        //     content: _t("Paso 2: Seleccionamos compañía muebleria srl"),
+        //     position: "bottom",
+        //     edition: "enterprise",
+        //     run: "click"
+        // },
+        // {
+        //     trigger: ".o_switch_company_menu > .dropdown-toggle",
+        //     content: _t("Paso 3: Abrir desplegable"),
+        //     position: "bottom",
+        //     edition: "enterprise",
+        //     run: "click"
+        // },
+        // {
+        //     trigger: ".fa-square-o:before",
+        //     content: _t("Paso 4: incorporamos otra compania"),
+        //     position: "bottom",
+        //     edition: "enterprise",
+        //     run: "click"
+        // },
         {
-            trigger: ".o_switch_company_menu",
-            content: _t("Paso 1: Seleccionar menu companias"),
-            position: "bottom",
-            edition: "enterprise",
-            run: "click"
+        trigger: ".o_menuitem[data-menu-xmlid='sale.sale_menu_root']",
+        content: _t("Open Sales app to send your first quotation in a few clicks."),
+        position: "bottom",
+        edition: "enterprise"
+    },
+    {
+        trigger: "button.btn-primary.o_list_button_add",
+        content: _t("step 2"),
+        position: "bottom",
+        edition: "enterprise",
+        run: "click"
+    }, {
+        trigger: ".o_field_res_partner_many2one[name='partner_id']",
+        extra_trigger: ".o_sale_order",
+        content: _t("Write a company name to create one, or see suggestions."),
+        position: "right",
+        run: function (actions) {
+            actions.text("Deco Addict", this.$anchor.find("input"));
         },
-        {
-            trigger: ".dropdown-toggle",
-            content: _t("Paso 12: Abrir desplegable"),
-            position: "bottom",
-            edition: "enterprise",
-            run: "click"
+    }, {
+        trigger: ".ui-menu-item > a:contains('Deco Addict')",
+        auto: true,
+        in_modal: false,
+    }, {
+        trigger: ".o_field_x2many_list_row_add > a",
+        content: _t("Click here to add some products or services to your quotation."),
+        position: "bottom",
+    }, {
+        trigger: ".o_field_widget[name='product_id'], .o_field_widget[name='product_template_id']",
+        extra_trigger: ".o_sale_order",
+        content: _t("Select a product, or create a new one on the fly."),
+        position: "right",
+        run: function (actions) {
+            var $input = this.$anchor.find("input");
+            actions.text("[FURN_8855] Drawer", $input.length === 0 ? this.$anchor : $input);
+            // fake keydown to trigger search
+            var keyDownEvent = jQuery.Event("keydown");
+            keyDownEvent.which = 42;
+            this.$anchor.trigger(keyDownEvent);
+            var $descriptionElement = $(".o_form_editable textarea[name='name']");
+            // when description changes, we know the product has been created
+            $descriptionElement.change(function () {
+                $descriptionElement.addClass("product_creation_success");
+            });
         },
-
-//         {
-//         trigger: ".o_menuitem[data-menu-xmlid='sale.sale_menu_root']",
-//         content: _t("Open Sales app to send your first quotation in a few clicks."),
-//         position: "bottom",
-//         edition: "enterprise"
-//     }, {
-//         trigger: "button.btn-primary.o_list_button_add",
-//         content: _t("step 2"),
-//         position: "bottom",
-//         edition: "enterprise",
-//         run: "click"
-//     }, {
-//         trigger: ".o_field_res_partner_many2one[name='partner_id']",
-//         extra_trigger: ".o_sale_order",
-//         content: _t("Write a company name to create one, or see suggestions."),
-//         position: "right",
-//         run: function (actions) {
-//             actions.text("Deco Addict", this.$anchor.find("input"));
-//         },
-//     }, {
-//         trigger: ".ui-menu-item > a:contains('Deco Addict')",
-//         auto: true,
-//         in_modal: false,
-//     }, {
-//         trigger: ".o_field_x2many_list_row_add > a",
-//         content: _t("Click here to add some products or services to your quotation."),
-//         position: "bottom",
-//     }, {
-//         trigger: ".o_field_widget[name='product_id'], .o_field_widget[name='product_template_id']",
-//         extra_trigger: ".o_sale_order",
-//         content: _t("Select a product, or create a new one on the fly."),
-//         position: "right",
-//         run: function (actions) {
-//             var $input = this.$anchor.find("input");
-//             actions.text("[E-COM11] Cabinet with Doors", $input.length === 0 ? this.$anchor : $input);
-//             // fake keydown to trigger search
-//             var keyDownEvent = jQuery.Event("keydown");
-//             keyDownEvent.which = 42;
-//             this.$anchor.trigger(keyDownEvent);
-//             var $descriptionElement = $(".o_form_editable textarea[name='name']");
-//             // when description changes, we know the product has been created
-//             $descriptionElement.change(function () {
-//                 $descriptionElement.addClass("product_creation_success");
-//             });
-//         },
-//         id: "product_selection_step"
-//     }, {
-//         trigger: "a:contains('[E-COM11] Cabinet with Doors')",
-//         auto: true,
-//     }, {
-//         trigger: ".o_field_widget[name='price_unit'] ",
-//         extra_trigger: ".fa-arrow-right",  // Wait for product creation
-//         content: Markup(_t("<b>Set a price</b>.")),
-//         position: "right",
-//         run: "text 10.0"
-//     }, {
-//         trigger: "button[name='action_confirm']",
-//         content: _t("step 3"),
-//         position: "bottom",
-//         edition: "enterprise",
-//         run: "click"
-//     },
+        id: "product_selection_step"
+    }, {
+        trigger: "a:contains('[FURN_8855] Drawer')",
+        auto: true,
+    }, {
+        trigger: ".o_field_widget[name='price_unit'] ",
+        extra_trigger: ".fa-arrow-right",  // Wait for product creation
+        content: Markup(_t("<b>Set a price</b>.")),
+        position: "right",
+        run: "text 10.0"
+    }, {
+        trigger: "button[name='action_confirm']",
+        content: _t("step 3"),
+        position: "bottom",
+        edition: "enterprise",
+        run: "click"
+    },
  ]
      tour.register("adhoc_tour_multicompany", {
          url: "/web",
