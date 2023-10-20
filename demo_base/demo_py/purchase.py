36f512c9108d7fa7f77409f4c478432c41b608dc
Juan José Scarafía <jjs@adhoc.com.ar>
2023-10-20 12:12:17 -0300

    [FIX] demo data sale, purchase and events

--- demo_base/demo_py/purchase.py
+++ demo_base/demo_py/purchase.py
@@ -17,9 +17,12 @@ class PurchaseOrder(models.Model):
         us_c = self.env.ref('base.main_company')
         for company in ar_c + uy_c + cl_c:
             for purchase in self.env['purchase.order'].search([('company_id', '=', us_c.id)]):
-                new_purchase = purchase.copy(default={'company_id': company.id})
-                new_purchase._compute_tax_id()
-                # TODO verificar si toma bien los impuestos
+                new_purchase = purchase.copy()
+                new_purchase.write({'company_id': company.id})
+                # este atributo existe solo si esta instalado purchase_stock
+                # en tal caso necesitamos llamarlo para que recompute picking type
+                if hasattr(new_purchase, '_onchange_company_id'):
+                    new_purchase._onchange_company_id()
                 if purchase.state == 'purchase':
                     new_purchase.button_confirm()
                 elif purchase.state == 'sent':
