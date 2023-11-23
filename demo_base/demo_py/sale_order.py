895139349b02afd495327d1ccc0249244b88ff40
Juan José Scarafia <jjs@adhoc.com.ar>
2023-11-23 01:39:06 +0000

    [FIX] demo_base: demo data for validity days
    closes ingadhoc/demo#34
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- demo_base/demo_py/sale_order.py
+++ demo_base/demo_py/sale_order.py
@@ -16,6 +16,11 @@ class SaleOrder(models.Model):
         # us (main_company)
         us_c = self.env.ref('base.main_company')
         for company in ar_c + uy_c + cl_c:
+            # setamos validity days de las companias a 100 por si está instalado sale_order_validity
+            # ya que en ese caso da error porque estamos duplicando ventas viejas (que está bueno queden con fecha
+            # vieja) y dan error al validar. De alguna manera hacemos lo mismo que hicimos en sale_order_validity
+            # con demo data (pero para el resto de las cias)
+            company.quotation_validity_days = 100
             new_sale_website_id = False
             new_sale_website_id = self.env['website'].search([('company_id', '=', company.id)], limit=1).id
             for sale in self.env['sale.order'].search([('company_id', '=', us_c.id)]):
